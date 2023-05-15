806a722f2f3254f035a6a93bc4bb736dbf6a0dfb
Julien Perrier <perrierjulien22@gmail.com>
2023-05-15 15:34:46 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MenuAppBar from "./components/appBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {" "}
+      <MenuAppBar></MenuAppBar>
+      <body>
+        <img src="/assets/images/FCS_team.png" alt="zef" />
+      </body>
+      <footer></footer>
     </div>
   );
 }
